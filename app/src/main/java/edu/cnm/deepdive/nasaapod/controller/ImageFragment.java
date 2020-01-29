128cc72e3571a7308912eaed9544b4d37db78281
Samuel Martinez <semartinez147@gmail.com>
2020-01-29 11:55:52 -0700

    Created MainViewModel to manage date and Retriever.  WIP.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/controller/ImageFragment.java
@@ -11,13 +11,16 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import edu.cnm.deepdive.nasaapod.BuildConfig;
 import edu.cnm.deepdive.nasaapod.R;
 import edu.cnm.deepdive.nasaapod.model.Apod;
 import edu.cnm.deepdive.nasaapod.service.ApodService;
+import edu.cnm.deepdive.nasaapod.viewmodel.MainViewModel;
 import java.io.IOException;
 import retrofit2.Response;
 import retrofit2.Retrofit;
@@ -26,8 +29,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ImageFragment extends Fragment {
 
   private static final String IMAGE_URL = "https://apod.nasa.gov/apod/image/2001/ic410_WISEantonucci_1824.jpg";
-  private WebView contentView;
 
+  private WebView contentView;
+  private MainViewModel viewModel;
+
+  @Override
   @SuppressLint("SetJavaScriptEnabled")
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
@@ -36,10 +42,16 @@ public class ImageFragment extends Fragment {
     return root;
   }
 
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    viewModel = new ViewModelProvider(getActivity()).get(MainViewModel.class);
+  }
+
   @SuppressLint("SetJavaScriptEnabled")
   private void setupWebView(View root) {
     contentView = root.findViewById(R.id.content_view);
-    contentView.setWebViewClient(new WebViewClient(){
+    contentView.setWebViewClient(new WebViewClient() {
       @Override
       public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
         return false;
@@ -60,33 +72,5 @@ public class ImageFragment extends Fragment {
     new Retriever().start();
   }
 
-  private class Retriever extends Thread {
 
-    @Override
-    public void run() {
-      Gson gson = new GsonBuilder()
-          .excludeFieldsWithoutExposeAnnotation()
-          .setDateFormat("yyyy-MM-dd")
-          .create();
-
-      Retrofit retrofit = new Retrofit.Builder()
-          .baseUrl(BuildConfig.BASE_URL)
-          .addConverterFactory(GsonConverterFactory.create(gson))
-          .build();
-
-      ApodService service = retrofit.create(ApodService.class);
-      try {
-        Response<Apod> response = service.get(BuildConfig.API_KEY, "2018-09-20").execute();
-        if (response.isSuccessful()){
-          Apod apod = response.body();
-          String url = apod.getUrl();
-          getActivity().runOnUiThread(() -> contentView.loadUrl(url));
-        } else {
-          Log.e("ApodService", response.message());
-        }
-      } catch (IOException e) {
-        Log.e("ApodService", e.getMessage(), e);
-      }
-    }
-  }
 }
