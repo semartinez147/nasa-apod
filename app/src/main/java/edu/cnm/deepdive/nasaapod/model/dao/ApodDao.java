a9d9c992d17fea79cf102788edffb5b90048c01e
Samuel Martinez <semartinez147@gmail.com>
2020-02-04 14:47:46 -0700

    Images are now saving to on-device database.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/model/dao/ApodDao.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/model/dao/ApodDao.java
@@ -1,32 +1,45 @@
 package edu.cnm.deepdive.nasaapod.model.dao;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import edu.cnm.deepdive.nasaapod.model.entity.Apod;
+import io.reactivex.Maybe;
+import io.reactivex.Single;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
 @Dao // Data Access Object
 public interface ApodDao {
-  // CRUD - Create, Read, Update, Delete
+  // CRUD - Create, Read, Update (but not here), Delete
 
-  @Insert //Create
-  long insert(Apod apod);
+  @Insert
+  Single<List<Long>> insert(Collection<Apod> apods);
 
-  @Delete //Delete
-  int delete(Apod apod); //returns # of records affected
+  @Insert
+  Single<Long> insert (Apod apod);
+
+  @Insert
+  Single<List<Long>> insert(Apod... apods);
 
   @Delete
-  int delete(Apod... apods);
+  Single<Integer> delete(Apod apod);
+
+  @Delete
+  Single<Integer> delete(Collection<Apod> apods);
+
+  @Delete
+  Single<Integer> delete(Apod... apods);
 
   @Query("SELECT * FROM Apod ORDER BY date DESC") //Read
-  List<Apod> select();
+  LiveData<List<Apod>> select();
 
   @Query("SELECT * FROM Apod WHERE date = :date") //"colon name" in SQL is a placeholder for a parameter
-  Apod select (Date date);
+  Maybe<Apod> select (Date date);
 
   @Query("SELECT * FROM Apod WHERE apod_id = :id")
-  Apod select (long id);
+  Single<Apod> select (long id);
 }
--- app/src/main/java/edu/cnm/deepdive/nasaapod/viewmodel/MainViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/viewmodel/MainViewModel.java
@@ -1,36 +1,40 @@
 package edu.cnm.deepdive.nasaapod.viewmodel;
 
-import android.annotation.SuppressLint;
 import android.app.Application;
-import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import edu.cnm.deepdive.nasaapod.BuildConfig;
+import edu.cnm.deepdive.nasaapod.model.dao.ApodDao;
 import edu.cnm.deepdive.nasaapod.model.entity.Apod;
+import edu.cnm.deepdive.nasaapod.service.ApodDatabase;
 import edu.cnm.deepdive.nasaapod.service.ApodService;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import io.reactivex.schedulers.Schedulers;
+import java.text.ParseException;
 import java.util.Date;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainViewModel extends AndroidViewModel {
 
   private Date apodDate;
   private MutableLiveData<Apod> apod;
   private MutableLiveData<Throwable> throwable;
+  private ApodDatabase database;
+  private ApodService nasa;
 
   public MainViewModel(@NonNull Application application) {
     super(application);
+    database = ApodDatabase.getInstance();
+    nasa = ApodService.getInstance();
     apod = new MutableLiveData<>();
     throwable = new MutableLiveData<>();
-    setApodDate(new Date()); // TODO investigate adjustment for NASA APoD-relevant timezone.
+    Date today = new Date();
+    String formattedDate = ApodService.DATE_FORMATTER.format(today);
+    try {
+      setApodDate(ApodService.DATE_FORMATTER.parse(formattedDate)); // TODO investigate adjustment for NASA APoD-relevant timezone.
+    } catch (ParseException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   public LiveData<Apod> getApod() {
@@ -42,41 +46,27 @@ public class MainViewModel extends AndroidViewModel {
   }
 
   public void setApodDate(Date date) {
-    apodDate = date;
-    new Retriever().start();
+    ApodDao dao = database.getApodDao(); // assign a method to a local variable when you know you'll be using it a lot.
+    dao.select(date)
+        .subscribeOn(Schedulers.io())
+        .subscribe(
+            (apod) -> this.apod.postValue(apod),
+            (throwable) -> this.throwable.postValue(throwable),
+            () -> nasa.get(BuildConfig.API_KEY, ApodService.DATE_FORMATTER.format(date))
+                .subscribeOn(Schedulers.io())
+                .subscribe(
+                    (apod) -> dao.insert(apod)
+                        .subscribeOn(Schedulers.io())
+                        .subscribe(
+                            (id) -> {
+                              apod.setId(id);
+                              this.apod.postValue(apod);
+                            },
+                            (throwable) -> this.throwable.postValue(throwable)
+                        ),
+                    (throwable) -> this.throwable.postValue(throwable)
+                )
+        );
   }
 
-  private class Retriever extends Thread {
-
-    private static final String DATE_FORMAT = "yyyy-MM-dd";
-
-    @Override
-    public void run() {
-      @SuppressLint("SimpleDateFormat") DateFormat format = new SimpleDateFormat(DATE_FORMAT);
-      String formattedDate = format.format(apodDate);
-      Gson gson = new GsonBuilder()
-          .excludeFieldsWithoutExposeAnnotation()
-          .setDateFormat("yyyy-MM-dd")
-          .create();
-
-      Retrofit retrofit = new Retrofit.Builder()
-          .baseUrl(BuildConfig.BASE_URL)
-          .addConverterFactory(GsonConverterFactory.create(gson))
-          .build();
-
-      ApodService service = retrofit.create(ApodService.class);
-      try {
-        Response<Apod> response = service.get(BuildConfig.API_KEY, formattedDate).execute();
-        if (response.isSuccessful()) {
-          Apod apod = response.body();
-          MainViewModel.this.apod.postValue(apod);
-        } else {
-          throw new RuntimeException(response.message());
-        }
-      } catch (IOException | RuntimeException e) {
-        Log.e("ApodService", e.getMessage(), e);
-        throwable.postValue(e);
-      }
-    }
-  }
 }
